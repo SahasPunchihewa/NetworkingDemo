995081b18acfda9bff91823eda76460ceb0f1292
Sahas <sahasmcg2995@gmail.com>
2024-10-18 19:39:24 +0530

    Initial Commit

---
+++ NetworkingDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkingDemo
+//
+//  Created by Sahas Punchihewa on 2024-10-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkingDemo/NetworkingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingDemoApp.swift
+//  NetworkingDemo
+//
+//  Created by Sahas Punchihewa on 2024-10-18.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
